e2c19ca0d21bafc9c5dde31eb860d08aec2a615b
yassine <yassine.nawara007@gmail.com>
2025-04-15 13:12:42 +0100

    updates final

--- src/Controller/EmployeController.php
+++ src/Controller/EmployeController.php
@@ -10,6 +10,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 
 #[Route('/employe')]
@@ -24,7 +25,7 @@ final class EmployeController extends AbstractController
     }
 
     #[Route('/new', name: 'app_employe_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    public function new(Request $request, EntityManagerInterface $em, UserPasswordHasherInterface $passwordHasher): Response
     {
         $employe = new Employe();
         $form = $this->createForm(EmployeType::class, $employe, [
